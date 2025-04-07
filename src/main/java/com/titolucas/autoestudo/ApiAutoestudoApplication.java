059932fa9a2fb1b90aac8572acea6a7eaafb98de
Lucas Tito <lucassga500@gmail.com>
2025-04-07 10:19:14 -0300

    first commit

---
+++ src/main/java/com/titolucas/autoestudo/ApiAutoestudoApplication.java
@@ -0,0 +1,13 @@
+package com.titolucas.autoestudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAutoestudoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAutoestudoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/titolucas/autoestudo/ApiAutoestudoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.titolucas.autoestudo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAutoestudoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
